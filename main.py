a10da2d6e7281d7f9008931348db7f8ce987669e
0xfuse <39297732+0xfuse@users.noreply.github.com>
2026-02-01 14:51:36 +0100

    Added server root var

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
-"""
-Python replacement for the original Bash script that scans a set of mod JARs
-until the first one that reproduces a crash is found.
 
-Author:  <your‑name>
-Date:    2024‑09‑13
+"""
+MIT License
+
+Copyright (c) 2026 0xfuse
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 import pathlib
@@ -17,7 +34,7 @@ from typing import Iterable, List, Optional
 # --------------------------------------------------------------------------- #
 #  CONFIGURATION  ----------------------------------------------------------- #
 # --------------------------------------------------------------------------- #
-SERVER_ROOT = "~/forge_server/"
+SERVER_ROOT = ""
 SERVER_SCRIPT = "start.sh"  # the script that starts the server
 ERROR_STR = (
     "Attempted to load class net/minecraft/client/gui/Gui for invalid dist DEDICATED_SERVER"
